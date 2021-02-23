f2fbcbfb382f8c5c4e9314164579128862a82081
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-02-23 14:47:23 +0400

    Update spacex_dag.py
    added generation parallel task for rocket

--- spacex_dag.py
+++ spacex_dag.py
@@ -21,7 +21,7 @@ t1 = BashOperator(
     bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
     dag=dag
 )
-
+'''
 t2 = BashOperator(
     task_id="print_data", 
     bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
@@ -30,3 +30,12 @@ t2 = BashOperator(
 )
 
 t1 >> t2
+'''
+for rocket in ('falcon1', 'falcon9', 'falconheavy'):
+    t2 = BashOperator(
+        task_id="print_data." + rocket, 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params={"rocket": rocket}, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
+    t1 >> t2
