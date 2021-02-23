380b48d805cbad7b96acbbfbc628ff019fa16b65
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-02-24 00:55:59 +0400

    Update spacex_dag.py
    fixed it error 5

--- spacex_dag.py
+++ spacex_dag.py
@@ -37,7 +37,7 @@ for rocket in ('falcon1', 'falcon9', 'falconheavy', 'all'):
     params = {"rocket": rocket} # falcon1/falcon9/falconheavy/all
     t1 = BashOperator(
         task_id="get_data." + rocket, 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data" + (" -r {{ params.rocket }}" if rocket !='all' else ""), 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data" + str(" -r {{ params.rocket }}" if rocket !='all' else ""), 
         #bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data{}".format(" -r {{ params.rocket }}" if rocket !='all' else ""),        
         params=params,
         dag=dag
