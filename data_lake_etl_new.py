0f89fd938ea5616a7b6a6da7c7117411eabab0a9
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-04-07 15:29:28 +0400

    Update data_lake_etl_new.py
    add job-name

--- data_lake_etl_new.py
+++ data_lake_etl_new.py
@@ -53,6 +53,8 @@ for task in tasks:
         task_id='ods_' + task,
         dag=dag,
         query=query,
+        job_name=username + '_{{ execution_date.year }}_ods_' + task + '_{{ params.job_suffix }}',
+        params={"job_suffix": randint(0, 100000)},
         cluster_name='cluster-dataproc',
         region='europe-west3-a',
     ))
@@ -65,6 +67,8 @@ dm = DataProcHiveOperator(
                select user_id, max(bytes_received), min(bytes_received), round(avg(bytes_received)) as avg_traf
                  from yfurman.ods_traffic where year = {{ execution_date.year }} group by user_id order by avg_traf;   
           """,
+    job_name=username + '_{{ execution_date.year }}_dm_traffic_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
     cluster_name='cluster-dataproc',
     region='europe-west3-a',
 )
